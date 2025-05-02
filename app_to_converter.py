418addfabf8377d6af73b642abfc4f849703adf2
Dylan Bustos Artavia <dylansba777@gmail.com>
2025-05-02 15:19:26 -0600

    Cambio 1

--- app_to_converter.py
+++ app_to_converter.py
@@ -173,5 +173,6 @@ def obtener_extra_costos(df):
     return f["Monto"].astype(float).sum()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
 
